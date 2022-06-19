fa40a28c7080d2bd36c7e5aa84f34a01fdac786c
Linh Vu <linh777gkog@gmail.com>
2022-06-19 23:31:03 +0700

    set random NUmber

--- Ball.js
+++ Ball.js
@@ -60,3 +60,12 @@ export default class Ball {
 function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min;
 }
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  );
+}
